73e32392f19b893e10c93e082f973f395bd2f517
Haritha1997 <100362096+Haritha1997@users.noreply.github.com>
2024-11-15 10:53:40 +0530

    Add files via upload

---
+++ src/org/nomus/Role.java
@@ -0,0 +1,8 @@
+package org.nomus;
+
+public class Role {
+	public static String SUPERADMIN="superadmin";
+	public static String ADMIN="admin";
+	public static String EDIT="edit";
+	public static String VIEW="view";
+}
